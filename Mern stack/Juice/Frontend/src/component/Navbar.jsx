fa1835aa6a922dc8c8d0693388ae2541da2859ba
Deepak213kk <deepak213k@gmail.com>
2026-02-18 00:57:50 +0530

    fix error

--- Mern stack/Juice/Frontend/src/component/Navbar.jsx
+++ Mern stack/Juice/Frontend/src/component/Navbar.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { FaShoppingCart, FaBars, FaTimes } from "react-icons/fa";
-import logo from "../assets/logo.png";
+import logo from "../assets/Logo.png";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./navbar.css";
 
